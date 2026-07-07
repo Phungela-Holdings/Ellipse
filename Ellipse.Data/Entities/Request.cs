6ef65b22d60426bbee36cd140f3ac82f7f3b27a6
MongeziSithole <MongeziSithole@MONGEZI>
2026-07-07 14:49:31 +0200

    Add project files.

---
+++ Ellipse.Data/Entities/Request.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ellipse.Data.Entities
+{
+    public class Request
+    {
+        public int Id { get; set; }
+
+        public string Status { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public DateTime RequestedDate { get; set; }
+
+        public Int64 MyProperty { get; set; }
+
+
+    }
+}
