aa5db78a00c5f22e9a247b7a6ce6f05b4ba5df45
Neliswa Mfeka <nmfeka@phungela.co.za>
2026-07-10 15:04:31 +0200

    Added Employee Table

--- Ellipse.Data/EllipseDbContext.cs
+++ Ellipse.Data/EllipseDbContext.cs
@@ -8,5 +8,6 @@ namespace Ellipse.Data
         public EllipseDbContext(DbContextOptions<EllipseDbContext> options) : base(options) { }
 
         public DbSet<Request> Requests { get; set; }
+        public DbSet<Employee> Employees { get; set; }
     }
 }
---
+++ Ellipse.Data/Entities/Employee.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ellipse.Data.Entities
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string Email_Address { get; set; }
+
+        public int PostId { get; set; }
+
+        public string Surname { get; set; }
+
+        public string First_Name { get; set; }
+
+        public string Contact_Number { get; set; }
+
+        public string Service_Number { get; set; }
+
+        public string Department { get; set; }
+
+        public string Branch { get; set; }
+
+        public string Designation { get; set; }
+
+        public string ActiveDirectortyUsername { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+}
