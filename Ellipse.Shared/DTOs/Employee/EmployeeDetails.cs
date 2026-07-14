478aba7f4be7256c7059bc144ed0d8db1d9d4da4
Neliswa Mfeka <nmfeka@phungela.co.za>
2026-07-14 15:18:58 +0200

    Added Interfaces and DOTs

---
+++ Ellipse.Shared/DTOs/Employee/EmployeeDetails.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ellipse.Shared.DTOs.Employee
+{
+    public class EmployeeDetails
+    {
+        public string EmailAddress { get; set; }
+
+        public int PostId { get; set; }
+
+        public string Surname { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string ContactNumber { get; set; }
+
+        public string ServiceNumber { get; set; }
+
+        public string Department { get; set; }
+
+        public string Branch { get; set; }
+
+        public string Designation { get; set; }
+
+        public string ActiveDirectortyUsername { get; set; }
+        //ToDo:Add request sumarries
+    }
+}
