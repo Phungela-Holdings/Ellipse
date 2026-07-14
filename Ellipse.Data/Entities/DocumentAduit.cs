5132fa7ba7ead6bb7abd4305f88b260d624ff1fe
MzwandileMngadi <mmngadi@phungela.co.za>
2026-07-14 13:43:07 +0200

    checking updates

---
+++ Ellipse.Data/Entities/DocumentAduit.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Ellipse.Data.Entities
+{
+    public class DocumentAudit
+    {
+        public int Id { get; set; }
+        public int DocumentId { get; set; }
+
+        [ForeignKey(nameof(DocumentId))]
+        public Document Document { get; set; }
+        public DateTime DateCreated {get; set; } 
+        public string AuditType { get; set; }
+        public Byte DocumentData { get; set; }
+        public Byte OldDocumentData { get; set; }
+    }
+}
---
+++ Ellipse.Data/Entities/Employee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Ellipse.Data.Entities
+{
+    public class Employee
+    {
+        [Key]
+        public string EmailAddress { get; set; }
+
+        public int PostId { get; set; }
+
+        public string Surname { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string ContactNumber { get; set; }
+
+        public string ServiceNumber { get; set; }
+
+        public string Department { get; set; }
+
+        public string Branch { get; set; }
+
+        public string Designation { get; set; }
+
+        public string ActiveDirectortyUsername { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public List<Request> Requests { get; set; }
+    }
+}
