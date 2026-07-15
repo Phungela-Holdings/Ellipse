d9d72c210157715275ce35cfb971ab75ffffbf0f
nmkhwanazi <nmkhwanazi@phungela.co.za>
2026-07-15 15:42:22 +0200

    ceated extension

---
+++ Ellipse.Core/Extensions/DocumentAuditExtension.cs
@@ -0,0 +1,15 @@
+﻿using Ellipse.Shared.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ellipse.Core.Extensions
+{
+    public static class DocumentAuditExtensions
+    {
+        public static IServiceCollection AddDocumentAuditService(this IServiceCollection services)
+        {
+            services.AddScoped<IDocumentAuditService, DocumentAuditService>();
+
+            return services;
+        }
+    }
+}
--- Ellipse.Core/Extensions/IServiceCollection_Extensions.cs
+++ Ellipse.Core/Extensions/IServiceCollection_Extensions.cs
@@ -1,23 +1,36 @@
-﻿using Ellipse.Core; // fixes missing reference to RequestServices
+﻿using Ellipse.Core;
 using Ellipse.Data;
 using Ellipse.Shared.Interfaces;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection; // fixes missing reference to RequestServicesusing Ellipse.Data;using Ellipse.Shared.Interfaces;using Microsoft.EntityFrameworkCore;using Microsoft.Extensions.Configuration;using Microsoft.Extensions.DependencyInjection;
 
 namespace Ellipse.Core.Extensions
+
 {
+
     public static class IServiceCollection_Extensions
     {
+
         public static void AddDbContext(this IServiceCollection services, IConfigurationManager Configuration)
+
         {
+            services.AddScoped<IDocumentAuditService, DocumentAuditService>();
+
             services.AddDbContext<EllipseDbContext>(options =>
+
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
         }
 
         public static void AddServices(this IServiceCollection services)
+
         {
+
             services.AddScoped<IRequestServices, RequestServices>();
+
         }
+
     }
+
 }
