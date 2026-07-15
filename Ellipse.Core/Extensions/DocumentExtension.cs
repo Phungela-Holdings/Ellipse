22f0af0a248991e25d3377313742a507aa36f542
Amahle0503 <acebekhulu@phungela.co.za>
2026-07-15 19:42:22 +0200

    Add Document extension methods

---
+++ Ellipse.Core/Extensions/DocumentExtension.cs
@@ -0,0 +1,45 @@
+﻿using Ellipse.Data.Entities;
+using DocumentDetails = Ellipse.Shared.DTOs.Document.Document;
+
+namespace Ellipse.Core.Extensions
+{
+    public static class DocumentExtension
+    {
+        public static DocumentDetails ToDetails(this Document document)
+        {
+            return new DocumentDetails
+            {
+                Id = document.Id,
+                Data = document.Data,
+                DateUploaded = document.DateUplouded,
+                DateModified = document.DateModified,
+                DocumentType = document.DocumentType,
+                RequestId = document.RequestId,
+                Active = document.IsActive,
+                Archived = document.Archived,
+                ArchivedDate = document.ArchivedDate
+            };
+        }
+
+        public static Document ToEntity(this DocumentDetails documentDetails)
+        {
+            return new Document
+            {
+                Id = documentDetails.Id,
+                Data = documentDetails.Data,
+                DateUplouded = documentDetails.DateUploaded,
+                DateModified = documentDetails.DateModified,
+                DocumentType = documentDetails.DocumentType,
+                RequestId = documentDetails.RequestId,
+                IsActive = documentDetails.Active,
+                Archived = documentDetails.Archived,
+                ArchivedDate = documentDetails.ArchivedDate
+            };
+        }
+
+        public static List<DocumentDetails> ToListDetails(this List<Document> documents)
+        {
+            return documents.Select(document => document.ToDetails()).ToList();
+        }
+    }
+}
