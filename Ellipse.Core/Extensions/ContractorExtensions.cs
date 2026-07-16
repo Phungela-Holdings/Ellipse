7f6ee51ec9f020a833d209488497ef1d6679aa97
MzwandileMngadi <mmngadi@phungela.co.za>
2026-07-16 10:41:09 +0200

    Added a Contractor Extension

---
+++ Ellipse.Core/Extensions/ContractorExtensions.cs
@@ -0,0 +1,57 @@
+﻿
+using Ellipse.Shared.DTOs.Contract;
+using Ellipse.Data.Entities;
+
+namespace Ellipse.Core.Extensions
+{
+    public static class ContractorExtensions
+    {
+        public static ContractorDetails ToDetails(this Contractor contractor)
+        {
+            return new ContractorDetails
+            {
+                Id = contractor.Id,
+                IdentificationNumber = contractor.IdentificationNumber,
+                Surname = contractor.Surname,
+                FirstName = contractor.FirstName,
+                EmailAddress = contractor.EmailAddress,
+                CompanyName = contractor.CompanyName,
+                ResponsibleManager = contractor.ResponsibleManager,
+                BusinessJustification = contractor.BusinessJustification,
+                StartDate = contractor.StartDate,
+                EndDate = contractor.EndDate,
+                Department = contractor.Department,
+                Branch = contractor.Branch
+            };
+        }
+        public static Contractor ToEntity(this ContractorDetails contractorDetails)
+        {
+            return new Contractor
+            {
+                Id = contractorDetails.Id,
+                IdentificationNumber = contractorDetails.IdentificationNumber,
+                Surname = contractorDetails.Surname,
+                FirstName = contractorDetails.FirstName,
+                EmailAddress = contractorDetails.EmailAddress,
+                CompanyName = contractorDetails.CompanyName,
+                ResponsibleManager = contractorDetails.ResponsibleManager,
+                BusinessJustification = contractorDetails.BusinessJustification,
+                StartDate = contractorDetails.StartDate,
+                EndDate = contractorDetails.EndDate,
+                Department = contractorDetails.Department,
+                Branch = contractorDetails.Branch
+            };
+        }
+
+        public static ContractorSummary ToSummary(this Contractor contractor)
+        {
+            return new ContractorSummary
+            {
+                Id = contractor.Id,
+                Surname = contractor.Surname,
+                FirstName = contractor.FirstName,
+                EmailAddress = contractor.EmailAddress,
+            };
+        }
+    }
+}
