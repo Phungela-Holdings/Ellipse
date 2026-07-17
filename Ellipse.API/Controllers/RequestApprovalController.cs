b48f4becaefa027740d71dd03dacc15ab4573bd5
MzwandileMngadi <mmngadi@phungela.co.za>
2026-07-17 14:56:58 +0200

    latest code

---
+++ Ellipse.API/Controllers/RequestApprovalController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ellipse.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RequestApprovalController : ControllerBase
+    {
+    }
+}
