2f3a5087dfae8ee7323098232c49ae2452395a23
Neliswa Mfeka <nmfeka@phungela.co.za>
2026-07-17 11:43:33 +0200

    Cleaning the code

--- Ellipse.Shared/DTOs/Employee/EmployeeDetails.cs
+++ Ellipse.Shared/DTOs/Employee/EmployeeDetails.cs
@@ -7,23 +7,14 @@ namespace Ellipse.Shared.DTOs.Employee
     public class EmployeeDetails
     {
         public string EmailAddress { get; set; }
-
         public int PostId { get; set; }
-
         public string Surname { get; set; }
-
         public string FirstName { get; set; }
-
         public string ContactNumber { get; set; }
-
         public string ServiceNumber { get; set; }
-
         public string Department { get; set; }
-
         public string Branch { get; set; }
-
         public string Designation { get; set; }
-
         public string ActiveDirectoryUsername { get; set; }
         //ToDo:Add request sumarries
     }
--- Ellipse.Shared/DTOs/Employee/EmployeeSummary.cs
+++ Ellipse.Shared/DTOs/Employee/EmployeeSummary.cs
@@ -7,9 +7,7 @@ namespace Ellipse.Shared.DTOs.Employee
     public class EmployeeSummary
     {
         public string EmailAddress { get; set; }
-
         public string Surname { get; set; }
-
         public string FirstName { get; set; }
         public string ServiceNumber { get; set; }
         public string Department { get; set; }
