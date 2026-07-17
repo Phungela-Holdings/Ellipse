0d4f2f78464c2baf61dc1373eed2f4ebe460c835
Neliswa Mfeka <nmfeka@phungela.co.za>
2026-07-17 15:30:31 +0200

    Adding EmployeeController

---
+++ Ellipse.API/Controllers/EmployeeController.cs
@@ -0,0 +1,82 @@
+﻿using Ellipse.Shared.DTOs.Employee;
+using Ellipse.Shared.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ellipse.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IEmployeeServices _employeeServices;
+        private readonly ILogger<EmployeeController> _logger;
+
+        public EmployeeController(
+            IEmployeeServices employeeService,
+            ILogger<EmployeeController> logger)
+        {
+            _employeeServices = employeeService;
+            _logger = logger;
+        }
+        [HttpPost]
+        public async Task<ActionResult<EmployeeDetails>> CreateEmployeeAsync(EmployeeDetails employeeDetails)
+        {
+            try
+            {
+                var result = await _employeeServices.CreateEmployeeAsync(employeeDetails);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogWarning(ex, "An error occured while creating employee.");
+                throw;
+            }
+        }
+        [HttpGet]
+        public async Task<ActionResult> GetEmployeeByIdAsync(string email)
+        {
+            try
+            {
+                var result = await _employeeServices.GetEmployeeByIdAsync(email);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogWarning(ex, "An error occured while retrieving employee email.");
+                throw;
+            }
+        }
+        [HttpPut]
+        public async Task<ActionResult> UpdateEmployeeAsync(EmployeeDetails employeeDetails)
+        {
+            try
+            {
+                var result = await _employeeServices.UpdateEmployeeAsync(employeeDetails);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogWarning(ex, "An error occured while updating employee.");
+                throw;
+            }
+        }
+        [HttpDelete]
+        public async Task<ActionResult> DeleteEmployeeAsync(string email)
+        {
+            try
+            {
+                var result = await _employeeServices.DeleteEmployeeAsync(email);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogWarning(ex, "An error occured while deleting an employee.");
+                throw;
+            }
+        }
+    }
+}
+
+    
+        
