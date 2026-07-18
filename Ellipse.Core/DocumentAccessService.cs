078a9a678371e66b8b1e2cfabe6254dbf3d7504e
Amahle0503 <acebekhulu@phungela.co.za>
2026-07-18 16:41:43 +0200

    Corrected errors on IDocumentService and created DocumentAccessController

--- Ellipse.Core/DocumentAccessService.cs
+++ Ellipse.Core/DocumentAccessService.cs
@@ -1,22 +1,75 @@
-using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Ellipse.Core.Extensions;
+using Ellipse.Data;
+using Ellipse.Data.Entities;
+using Ellipse.Shared.DTOs;
+using Ellipse.Shared.Interfaces;
 
-namespace Ellipse.Shared.DTOs.DocumentAccess
+namespace Ellipse.Core
 {
-    public class DocumentAccessDetails
+    public class DocumentAccessService : IDocumentAccessService
     {
-        public int Id { get; set; }
-        public int DocumentId { get; set; }
-        public string AccessedBy { get; set; } = string.Empty;
-        public DateTime DateAccessed { get; set; }
-    }
+        private readonly EllipseDbContext _context;
 
-    public class DocumentAccessSearchCriteria
-    {
-        public int? DocumentId { get; set; }
-        public string? AccessedBy { get; set; }
-        public DateTime? DateAccessedFrom { get; set; }
-        public DateTime? DateAccessedTo { get; set; }
-        public int PageNumber { get; set; }
-        public int PageSize { get; set; }
+        public DocumentAccessService(EllipseDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<DocumentAccessDetails> CreateDocumentAccessAsync(DocumentAccessDetails documentAccessDetails)
+        {
+            var entity = documentAccessDetails.ToEntity();
+
+            entity.DateAccessed = entity.DateAccessed == default
+                ? DateTime.UtcNow
+                : entity.DateAccessed;
+
+            _context.Set<DocumentAccess>().Add(entity);
+
+            await _context.SaveChangesAsync();
+
+            return entity.ToDetails();
+        }
+
+        public async Task<List<DocumentAccessDetails>> GetDocumentAccessAsync(int documentId)
+        {
+            var documentAccesses = await _context.Set<DocumentAccess>()
+                .Where(documentAccess => documentAccess.DocumentId == documentId)
+                .ToListAsync();
+
+            return documentAccesses.ToListDetails();
+        }
+
+        public async Task<List<DocumentAccessDetails>> SearchDocumentAccessAsync(DocumentAccessSearchCriteria searchCriteria)
+        {
+            var query = _context.Set<DocumentAccess>().AsQueryable();
+
+            query = searchCriteria.DocumentId.HasValue
+                ? query.Where(documentAccess => documentAccess.DocumentId == searchCriteria.DocumentId.Value)
+                : query;
+
+            query = string.IsNullOrWhiteSpace(searchCriteria.AccessedBy)
+                ? query
+                : query.Where(documentAccess => documentAccess.AccessedBy == searchCriteria.AccessedBy);
+
+            query = searchCriteria.DateAccessedFrom.HasValue
+                ? query.Where(documentAccess => documentAccess.DateAccessed >= searchCriteria.DateAccessedFrom.Value)
+                : query;
+
+            query = searchCriteria.DateAccessedTo.HasValue
+                ? query.Where(documentAccess => documentAccess.DateAccessed <= searchCriteria.DateAccessedTo.Value)
+                : query;
+
+            var pageNumber = searchCriteria.PageNumber <= 0 ? 1 : searchCriteria.PageNumber;
+
+            var pageSize = searchCriteria.PageSize <= 0 ? 10 : searchCriteria.PageSize;
+
+            var documentAccesses = await query
+                .Skip((pageNumber - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync();
+
+            return documentAccesses.ToListDetails();
+        }
     }
 }
---
+++ Ellipse.Shared/Interfaces/IDocumentAccessService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Ellipse.Shared.DTOs;
+
+namespace Ellipse.Shared.Interfaces
+{
+    public interface IDocumentAccessService
+    {
+        Task<DocumentAccessDetails> CreateDocumentAccessAsync(DocumentAccessDetails documentAccessDetails);
+
+        Task<List<DocumentAccessDetails>> GetDocumentAccessAsync(int documentId);
+
+        Task<List<DocumentAccessDetails>> SearchDocumentAccessAsync(DocumentAccessSearchCriteria searchCriteria);
+    }
+}
