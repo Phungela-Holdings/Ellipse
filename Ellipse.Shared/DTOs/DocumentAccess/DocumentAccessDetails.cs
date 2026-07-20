38e7430641db9469a9dae2a9921f4a2e86010c6d
Mongezi Sithole <msithole@phungela.co.za>
2026-07-20 10:50:01 +0200

    Fixed errors caused by merge

---
+++ Ellipse.Shared/DTOs/DocumentAccess/DocumentAccessDetails.cs
@@ -0,0 +1,10 @@
+﻿namespace Ellipse.Shared.DTOs.DocumentAccess
+{
+    public class DocumentAccessDetails
+    {
+        public int Id { get; set; }
+        public int DocumentId { get; set; }
+        public string AccessedBy { get; set; } = string.Empty;
+        public DateTime DateAccessed { get; set; }
+    }
+}
