4dfe395d33f87cd66e767f9737a08d30cad3eb79
Amahle0503 <acebekhulu@phungela.co.za>
2026-07-22 13:10:35 +0200

    Changed EnvironmentId to Id

--- Ellipse.Data/Entities/Environment.cs
+++ Ellipse.Data/Entities/Environment.cs
@@ -8,7 +8,7 @@ namespace Ellipse.Data.Entities
     public class Environment
     {
         [Key]
-        public int EnvironmentId { get; set; }
+        public int Id { get; set; }
 
         [Required]
         [MaxLength(20)]
